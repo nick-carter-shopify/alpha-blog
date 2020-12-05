351791f4ee31e5738dc9a27abbdedf85af31cb72
Nick Carter <nick.carter@shopify.com>
2020-12-05 13:20:21 +1300

    add validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
-
+  # validation
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  INVALID_EMAIL_MESSAGE = "Must be valid email address"
+  
+  validates :username, presence: true, 
+                       length: { minimum: 3, maximum: 25 }, 
+                       uniqueness: { case_senstive: false }
+  validates :email, presence: true, 
+                    length: { maximum: 105 }, 
+                    uniqueness: { case_sensitive: false }, 
+                    format: { with: VALID_EMAIL_REGEX, message: INVALID_EMAIL_MESSAGE }
 end
